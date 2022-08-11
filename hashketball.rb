1fbfa21d85c6effbeaaf57cc3a678ba640da16cb
Nicholas <nicholasjoven@gmail.com>
2022-08-11 01:50:53 -0400

    completed lab

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,88 @@ def game_hash
 end
 
 # Write code here
+
+# Structure:
+# 1st matrix: {home: , away: ,}
+# 2nd matrix home: {team_name: , colors: [], players: []}
+# 3rd matrix players[{}]
+# 4th matrix (element) {player_name: , etc} 
+
+# step 1: remove division of home and array to get 1-d array of players
+
+def both_rosters
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+# step 2: create a method to find the argument
+
+# expect(num_points_scored("Jeff Adrien")).to eq(10)
+
+# .find: enumerable.find { |iteration| condition } #=> 
+# returns the first element where condition == true
+
+
+def num_points_scored(player)
+  get_player(player)[:points] # both_rosters.find { |player| player[:player_name] == player_search }
+end
+
+# note that you can abstract target to its own function
+
+def get_player(player_search)
+  both_rosters.find { |player| player[:player_name] == player_search }
+end
+
+def shoe_size(player)
+  get_player(player)[:shoe]
+end
+
+def team_colors(name)
+  get_team(name)[:colors]
+end
+
+def team_names
+  game_hash.map { |home_or_away, info| info[:team_name]}
+end
+
+def player_numbers(team)
+  get_team(team)[:players].map { |player| player[:number] }
+end
+
+def player_stats(player_search)
+  both_rosters.find { |player| player[:player_name] == player_search}
+end
+
+# Get team
+
+def get_team(team_search)
+  game_hash.find { |home_or_away, info| info[:team_name] == team_search }[1] # finds hash either home: {} or away: {}, since we want the {} of either pair we specify [1]...
+end
+
+def big_shoe_rebounds
+  both_rosters.max_by { |player| player[:shoe] }[:rebounds]
+end
+
+#  [
+#   {
+#     :player_name=>"Alan Anderson", 
+#     :number=>0, 
+#     :shoe=>16, 
+#     :points=>22, 
+#     :rebounds=>12, 
+#     :assists=>12, 
+#     :steals=>3, 
+#     :blocks=>1, 
+#     :slam_dunks=>1
+#   }, 
+#   {
+#     :player_name=>"Reggie Evans", 
+#     :number=>30, 
+#     :shoe=>14, 
+#     :points=>12, 
+#     :rebounds=>12, 
+#     :assists=>12, 
+#     :steals=>12, 
+#     :blocks=>12, 
+#     :slam_dunks=>7
+#   }, 
+#   {:player_name=>"Brook Lopez", :number=>11, :shoe=>17, :points=>17, :rebounds=>19, :assists=>10, :steals=>3, :blocks=>1, :slam_dunks=>15}, {:player_name=>"Mason Plumlee", :number=>1, :shoe=>19, :points=>26, :rebounds=>11, :assists=>6, :steals=>3, :blocks=>8, :slam_dunks=>5}, {:player_name=>"Jason Terry", :number=>31, :shoe=>15, :points=>19, :rebounds=>2, :assists=>2, :steals=>4, :blocks=>11, :slam_dunks=>1}, {:player_name=>"Jeff Adrien", :number=>4, :shoe=>18, :points=>10, :rebounds=>1, :assists=>1, :steals=>2, :blocks=>7, :slam_dunks=>2}, {:player_name=>"Bismack Biyombo", :number=>0, :shoe=>16, :points=>12, :rebounds=>4, :assists=>7, :steals=>22, :blocks=>15, :slam_dunks=>10}, {:player_name=>"DeSagna Diop", :number=>2, :shoe=>14, :points=>24, :rebounds=>12, :assists=>12, :steals=>4, :blocks=>5, :slam_dunks=>5}, {:player_name=>"Ben Gordon", :number=>8, :shoe=>15, :points=>33, :rebounds=>3, :assists=>2, :steals=>1, :blocks=>1, :slam_dunks=>0}, {:player_name=>"Kemba Walker", :number=>33, :shoe=>15, :points=>6, :rebounds=>12, :assists=>12, :steals=>7, :blocks=>5, :slam_dunks=>12}]
